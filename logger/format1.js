8db99242586de533cc6715d6ec61507f56827f97
naman2003now <naman2003now.com>
2022-10-14 12:22:33 +0530

    Line Numbers

--- logger/format1.js
+++ logger/format1.js
@@ -1,32 +1,56 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf, json, prettyPrint } = format;
-const line = require("get-current-line");
+const fileInfo = require("get-current-line")
+
+class MyLogger {
+  constructor() {
+    this.logger = createLogger({
+      level: "debug",
+      format: combine(
+        timestamp(),
+        json(),
+        prettyPrint()
+      ),
+      defaultMeta: { service: "user-service" },
+      transports: [
+        new transports.Console({
+          depth: true,
+        }),
+        new transports.File({
+          filename: "errors.log",
+        }),
+        new transports.File({
+          level: "error",
+          filename: "./logs/example-3.log",
+        }),
+      ],
+    });
+  }
+
+  getFileInfo() {
+    let { line, method, file } = fileInfo.default({ frames: 3 })
+    return { line, method, file }
+  }
+
+  info(str) {
+    this.logger.info(str, this.getFileInfo(), __dirname)
+  }
+
+  warn(str) {
+    this.logger.info(str, this.getFileInfo())
+  }
+
+  error(str) {
+    this.logger.info(str, this.getFileInfo())
+  }
+
+  debug(str) {
+    this.logger.info(str, this.getFileInfo())
+  }
+}
 
 const format1 = () => {
-  return createLogger({
-    level: "debug",
-    format: combine(
-      timestamp(),
-      json(),
-      prettyPrint()
-      // printf((info) => {
-      //   return line.default().line;
-      // })
-    ),
-    defaultMeta: { service: "user-service" },
-    transports: [
-      new transports.Console({
-        depth: true,
-      }),
-      new transports.File({
-        filename: "errors.log",
-      }),
-      new transports.File({
-        level: "error",
-        filename: "./logs/example-3.log",
-      }),
-    ],
-  });
+  return new MyLogger();
 };
 
 module.exports = format1;
--- logger/format2.js
+++ logger/format2.js
@@ -1,27 +1,37 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf } = format;
 
+class MyLogger {
+  constructor() {
+    const myFormat = printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    });
+    this.logger = createLogger({
+      level: "debug",
+      format: combine(
+        format.colorize(),
+        label({ label: "right meow!" }),
+        timestamp({ format: "HH:mm:ss" }),
+        myFormat
+      ),
+
+      transports: [
+        new transports.Console(),
+        new transports.File({
+          filename: "errors.log",
+        }),
+      ],
+    });
+  }
+
+  info(str) {
+    this.logger.info(str, "Line: " + require("get-current-line").default(-1))
+  }
+}
+
 const format2 = () => {
-  const myFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-  });
-
-  return createLogger({
-    level: "debug",
-    format: combine(
-      format.colorize(),
-      label({ label: "right meow!" }),
-      timestamp({ format: "HH:mm:ss" }),
-      myFormat
-    ),
-
-    transports: [
-      new transports.Console(),
-      new transports.File({
-        filename: "errors.log",
-      }),
-    ],
-  });
+  return new MyLogger()
 };
+
 
 module.exports = format2;
